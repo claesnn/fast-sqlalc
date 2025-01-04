a1c2c9714c0fa4275d85aba1db18fe14520d3607
Claes Nymand Nilsson <claes.n.nilsson@gmail.com>
2025-01-04 13:39:41 +0100

    Use mixins

--- models.py
+++ models.py
@@ -8,23 +8,31 @@ class Base(MappedAsDataclass, DeclarativeBase):
     """Base class for all models."""
 
 
-class User(Base):
+class Mixin(MappedAsDataclass):
+    """Mixin for all models."""
+
+
+class IdMixin(Mixin):
+    """Mixin for all models with an id."""
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+
+class User(Base, IdMixin):
     """User model."""
 
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
 
 
-class Post(Base):
+class Post(Base, IdMixin):
     """Post model."""
 
     __tablename__ = "posts"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     content: Mapped[str]
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
